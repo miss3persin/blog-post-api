e1e021c1335c8c31d9090927bb6120e1b4b6d9aa
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2023-08-13 17:08:16 +0100

    ...(*sigh) another deployment fix

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const express = require("express");
 const faker = require("faker");
+
+const server = express();
+
 
 function generateComments() {
   const numComments = Math.floor(Math.random() * 7);
@@ -15,6 +19,7 @@ function generateComments() {
   return comments;
 }
 
+
 function generateBlogPost(id) {
   return {
     id: id,
@@ -29,6 +34,7 @@ function generateBlogPost(id) {
   };
 }
 
+
 const blogPostAmount = (count) => {
   const blogPosts = [];
 
@@ -38,4 +44,35 @@ const blogPostAmount = (count) => {
   return blogPosts;
 };
 
-module.exports = { blogPostAmount };
+
+const customRootMiddleware = (req, res, next) => {
+  if (req.url === "/") {
+    const message = '<h1>This is a Blog Posts API.</h1><h4>For a default number of blog posts (10) add "/posts" to the base URL.<br><br>For a preferred number of blog posts add "/posts?count=[insert amount you want]" to the base URL.</h4>';
+    res.send(message);
+  } else {
+    next();
+  }
+};
+
+server.use(express.json());
+server.use(customRootMiddleware);
+
+
+
+server.get("/posts", (req, res) => {
+  const count = parseInt(req.query.count) || 10;
+  const blogPosts = blogPostAmount(count);
+  
+  res.jsonp(blogPosts);
+});
+
+
+server.use((req, res, next) => {
+  res.status(404).send('<h1>Not an endpoint</h1>');
+});
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT, () => {
+  console.log(`Express server is running on port ${PORT}`);
+});
